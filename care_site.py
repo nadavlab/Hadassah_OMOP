bc0cc8faa5187da4d4f418080e2ef4be1f40b72a
tomdu <46071208+tomdua@users.noreply.github.com>
2022-03-10 08:56:58 +0200

    Update care_site.py

--- care_site.py
+++ care_site.py
@@ -1,7 +1,8 @@
 # Care site table
 #
 # Parameters:
-#
+#inpatient_visit_concept_id - 9201
+#initial_index - 1
 # Tables:
 # source table = transition Between departments
 # OMP table - care_site
@@ -10,11 +11,13 @@
 import pandas as pd
 
 table_name = "92.csv"
+inpatient_visit_concept_id = 9201
+initial_index = 1
 table = pd.read_csv(table_name)
 department = table["Department"].drop_duplicates()
 data=[]
-care_site_id = 1
-place_of_service_concept_id = 9201
+care_site_id = initial_index
+place_of_service_concept_id = inpatient_visit_concept_id
 for i, department_name in department.items():
     data.append([care_site_id, department_name, place_of_service_concept_id])
     care_site_id += 1
