9e55eed8d92b53c04ea40858bca158509847ac7b
tomdu <46071208+tomdua@users.noreply.github.com>
2022-04-09 19:27:06 +0300

    Update script_mimic.py

--- script_mimic.py
+++ script_mimic.py
@@ -1,17 +1,15 @@
-
 import pandas as pd
 from sqlalchemy import create_engine , text
 import os
 
-
 user = 'postgres'
-password = ''
+password =
 hostname = '132.72.65.168'
 database_name = 'Hadassah'
 
 engine = create_engine(f'postgresql+psycopg2://{user}:{password}@{hostname}/{database_name}', echo=True)
 
-path = r'C:\Users\ayalon\AppData\Local\Packages\CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc\LocalState\rootfs\home\ayalonaz\mimic-omop\1_omop_data_csv'
+path = r"C:\Users\Tom\"
 files = os.listdir(path)
 
 for file in files:
@@ -20,12 +18,11 @@ for file in files:
 
     try:
         table_name = file[:-4]
-        l = [x for x in df.keys() if  df[x].dtype.name == 'int64']
+        l = [x for x in df.keys() if df[x].dtype.name == 'int64']
         with engine.connect() as conn :
             for c in l:
-               conn.execute(text(f'ALTER TABLE mimic.{table_name} ALTER COLUMN {c} SET DATA TYPE BIGINT;'))
+               conn.execute(text(f'ALTER TABLE hadassah_omop2.{table_name} ALTER COLUMN {c} SET DATA TYPE BIGINT;'))
 
-        df.to_sql(file[:-4],index=False, con=engine, schema='mimic', if_exists='append')
+        df.to_sql(file[:-4], index=False, con=engine, schema='hadassah_omop2', if_exists='append')
     except Exception as e:
         print(e)
-
