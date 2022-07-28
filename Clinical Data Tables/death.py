0ccac71d4c769f8f17b23806d827910bb173f442
tduany <tom.duany@mobiletornado.com>
2022-07-28 21:10:50 +0300

    add if -  death equal to yes

--- Clinical Data Tables/death.py
+++ Clinical Data Tables/death.py
@@ -28,36 +28,37 @@ def is_exsit_person_id(id_baznat):
         return False
 
 for index_row, row in source_table.iterrows():
-    if is_exsit_person_id(row['ID_BAZNAT']):
-        person_id=row['ID_BAZNAT']
-    else:
-        continue
+    if (row['לידת מת'] == 'כן'):
+        if is_exsit_person_id(row['ID_BAZNAT']):
+            person_id=row['ID_BAZNAT']
+        else:
+            continue
 
-    try:
-        date = datetime.strptime(row["זמן לידה"] , '%m/%d/%Y %H:%M')
-    except:
-        date = datetime.strptime(row["זמן לידה"] , '%m/%d/%Y %H:%M:%S %p')
+        try:
+            date = datetime.strptime(row["זמן לידה"] , '%m/%d/%Y %H:%M')
+        except:
+            date = datetime.strptime(row["זמן לידה"] , '%m/%d/%Y %H:%M:%S %p')
 
-    death_date = date.date()
-    death_datetime = date
-    death_type_concept_id = EHR_concept_id
-    if row['סוג מוות'] == 'Intrauterine fetal death':
-        cause_concept_id = DeathType.INTRAUTERINE_FETAL_DEATH.value
-    elif row['סוג מוות'] == 'Fetal death due to termination of pregnancy':
-        cause_concept_id = DeathType.FETAL_DEATH_DUE_TO_TERMINATION_OF_PREGNANCY.value
-    elif row['סוג מוות'] == 'Intrapartum Fetal Death':
-        cause_concept_id = DeathType.INTRAPARTUM_FETAL_DEATH.value
-    elif row['סוג מוות'] == 'Early neonatal death':
-        cause_concept_id = DeathType.EARLY_NEONATAL_DEATH.value
-    elif row['סוג מוות'] == 'Selective Fetecide':
-        cause_concept_id = DeathType.SELECTIVE_FETECIDE.value
-    else:
-        cause_concept_id = DeathType.INTRAUTERINE_DEATH_OF_ONE_TWIN.value
+        death_date = date.date()
+        death_datetime = date
+        death_type_concept_id = EHR_concept_id
+        if row['סוג מוות'] == 'Intrauterine fetal death':
+            cause_concept_id = DeathType.INTRAUTERINE_FETAL_DEATH.value
+        elif row['סוג מוות'] == 'Fetal death due to termination of pregnancy':
+            cause_concept_id = DeathType.FETAL_DEATH_DUE_TO_TERMINATION_OF_PREGNANCY.value
+        elif row['סוג מוות'] == 'Intrapartum Fetal Death':
+            cause_concept_id = DeathType.INTRAPARTUM_FETAL_DEATH.value
+        elif row['סוג מוות'] == 'Early neonatal death':
+            cause_concept_id = DeathType.EARLY_NEONATAL_DEATH.value
+        elif row['סוג מוות'] == 'Selective Fetecide':
+            cause_concept_id = DeathType.SELECTIVE_FETECIDE.value
+        else:
+            cause_concept_id = DeathType.INTRAUTERINE_DEATH_OF_ONE_TWIN.value
 
-    cause_source_value = row['סוג מוות']
-    cause_source_concept_id = ''
+        cause_source_value = row['סוג מוות']
+        cause_source_concept_id = ''
 
-    data.append([person_id,death_date,death_datetime,death_type_concept_id,cause_concept_id,cause_source_value,cause_source_concept_id])
+        data.append([person_id,death_date,death_datetime,death_type_concept_id,cause_concept_id,cause_source_value,cause_source_concept_id])
 
 df_result = pd.DataFrame(data, columns=["person_id","death_date","death_datetime","death_type_concept_id","cause_concept_id","cause_source_value","cause_source_concept_id"])
 df_result.to_csv('death.csv', encoding='utf-8', index=False)
